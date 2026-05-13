2e6d7873cc10441e2f52c9099ae027c41cbbb972
Mrigakshi-RC <mrigakshirc@gmail.com>
2026-05-13 06:58:32 +0000

    add: create exchanges and queues from client and server

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,5 +1,59 @@
+import amqp from "amqplib";
+import { clientWelcome, commandStatus, getInput, printClientHelp, printQuit } from "../internal/gamelogic/gamelogic.js";
+import { declareAndBind, SimpleQueueType } from "../internal/pubsub/declareandbind.js";
+import { ExchangePerilDirect, PauseKey } from "../internal/routing/routing.js";
+import { GameState } from "../internal/gamelogic/gamestate.js";
+import { commandSpawn } from "../internal/gamelogic/spawn.js";
+import { commandMove } from "../internal/gamelogic/move.js";
+
 async function main() {
   console.log("Starting Peril client...");
+  const rabbitConnString = "amqp://guest:guest@localhost:5672/";
+  const conn = await amqp.connect(rabbitConnString);
+  console.log("Connection successful...");
+
+  process.on('exit', (code) => {
+    console.log('Process exit event with code: ', code);
+    conn.close();
+  });
+
+  const username = await clientWelcome();
+  const [channel, queue] = await declareAndBind(conn, ExchangePerilDirect, `pause.${username}`, PauseKey, SimpleQueueType.Transient)
+
+  const gameState = new GameState(username)
+  while (true) {
+    const inputArr = await getInput();
+    if (inputArr.length === 0) continue;
+    try {
+      switch (inputArr[0]) {
+        case "spawn":
+          commandSpawn(gameState, inputArr);
+          break;
+        case "move":
+          commandMove(gameState, inputArr);
+          console.log("Move command processed.");
+          break;
+        case "status":
+          commandStatus(gameState);
+          break;
+        case "help":
+          printClientHelp();
+          break;
+        case "spam":
+          console.log("Spamming not allowed yet!")
+          break;
+        case "quit":
+          printQuit()
+          process.exit(1);
+        default:
+          console.log("I don't understand your command")
+      }
+    }
+    catch (err) {
+      console.log("Error processing your command:", err instanceof Error ? err.message : err);
+    }
+
+  }
 }
 
 main().catch((err) => {
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,19 +1,44 @@
 import amqp from "amqplib";
 import { publishJSON } from "../internal/pubsub/publish.js";
-import { ExchangePerilDirect, PauseKey } from "../internal/routing/routing.js";
+import { ExchangePerilDirect, ExchangePerilTopic, GameLogSlug, PauseKey } from "../internal/routing/routing.js";
+import { getInput, printServerHelp } from "../internal/gamelogic/gamelogic.js";
+import { declareAndBind, SimpleQueueType } from "../internal/pubsub/declareandbind.js";
 
 async function main() {
+  console.log("Starting Peril server...");
+  printServerHelp();
   const rabbitConnString = "amqp://guest:guest@localhost:5672/";
   const conn = await amqp.connect(rabbitConnString);
   console.log("Connection successful...");
 
-  const channel = await conn.createConfirmChannel()
-  publishJSON(channel, ExchangePerilDirect, PauseKey, { isPaused: true })
+  const publishCh = await conn.createConfirmChannel()
 
   process.on('exit', (code) => {
     console.log('Process exit event with code: ', code);
     conn.close();
   });
+
+  await declareAndBind(conn, ExchangePerilTopic, GameLogSlug, "game_logs.*", SimpleQueueType.Durable)
+
+  while (true) {
+    const inputArr = await getInput();
+    if (inputArr.length === 0) continue;
+    switch (inputArr[0]) {
+      case "pause":
+        console.log("Sending a pause message");
+        publishJSON(publishCh, ExchangePerilDirect, PauseKey, { isPaused: true });
+        break;
+      case "resume":
+        console.log("Sending a pause message");
+        publishJSON(publishCh, ExchangePerilDirect, PauseKey, { isPaused: false });
+        break;
+      case "quit":
+        console.log("Exiting:");
+        process.exit(1);
+      default:
+        console.log("I don't understand your command")
+    }
+  }
 }
 
 main().catch((err) => {
